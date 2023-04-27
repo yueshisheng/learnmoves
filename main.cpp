8a6099dfa1552b52384e3917b700beca104e2299
yueshisheng <2298672896@qq.com>
2023-04-27 10:27:13 +0800

    完美转发

--- main.cpp
+++ main.cpp
@@ -30,11 +30,11 @@ public:
     //移动赋值函数
     base& operator=(base&& rbase)noexcept{
         cout << "移动赋值函数" << endl;
-            if(this->m_data!= nullptr) {
+
                 this->m_data = rbase.m_data;
                 rbase.m_data = nullptr;
                 return *this;
-            }
+
 
 
 
@@ -57,12 +57,14 @@ static base getbase(){
 
 int main() {
 
-    int data=100;
+    int data=10000;
     int data2=200;
     int data3=300;
+    int data4=500;
     int* dataptr=&data;
     int* dataptr2=&data2;
     int* dataptr3=&data3;
+    int* dataptr4=&data4;
     base base1(dataptr);
     base base2(base1);
     base base6(dataptr3);
@@ -71,8 +73,14 @@ int main() {
     //通过move函数，将左值引用转换为右值引用
     base base3(dataptr2);
     base base4(dataptr2);
-    base4=move(getbase());
+    base4=move(base2);
     cout<<*base4.m_data<<endl;
+
+    //完美转发
+    base&& base5(dataptr4);
+    base perfect;
+    perfect=forward<base>(base5);
+    cout<<*perfect.m_data<<endl;
 
 
 
